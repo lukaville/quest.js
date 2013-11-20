86d037f974061126b749e41bf81e691068e7d760
Nickolay Chameyev <lukaville@gmail.com>
2013-11-20 17:06:22 +0400

    Question showing, checking answer

--- js/quest.js
+++ js/quest.js
@@ -1,6 +1,51 @@
-var text = "Next level!";
-var pass = "key";
+//console.log(generateQuest(making));
 
-var encrypted = GibberishAES.enc(text, pass);
+function generateQuest(making) {
+    cryptedQuestion = "-";
+    var questionObject;
+    for (i = making.length - 1; i >= 0; i--) {
+        questionObject = {
+            heading: making[i].heading,
+            question: making[i].question,
+            nextQuestion: cryptedQuestion,
+            questionIndex: (i)
+        }
+        cryptedQuestion = GibberishAES.enc(JSON.stringify(questionObject), making[i].answer);
+    }
+    return JSON.stringify(questionObject);
+}
 
-console.log(encrypted)
+function addQuestionDiv(heading, question, index) {
+    questionsDiv = $("#questions");
+
+    html = '<div class="quiz"><div class="quiz-heading">' + heading + '</div><div class="quiz-content">' + question + '</div><div class="quiz-answer"><input class="answer" type="text" /><button class="quiz-button" onclick="answerQuestion('+ index +');">Проверить</button></div></div>';
+
+    questionsDiv.append(html);
+}
+
+function startQuest() {
+    addQuestionDiv(quest.heading, quest.question, quest.questionIndex);
+}
+
+function answerQuestion(index) {
+    questionDiv = $("#questions").find(".quiz")[index];
+    answerInput = $(questionDiv).find(".answer")[0];
+    key = answerInput.value;
+
+    try {
+        text = GibberishAES.dec(quest.nextQuestion, key);
+
+        if (text == "WIN") {
+            alert("Победа!");
+        } else {
+            quest = JSON.parse(GibberishAES.dec(quest.nextQuestion, key));
+            //$(questionDiv).find(".answer")[0].find(".quiz-button").attr('disabled','disabled');
+            addQuestionDiv(quest.heading, quest.question, quest.questionIndex);
+        }
+        
+    } catch(error) {
+        alert("Неправильный ответ!");
+    }
+}
+
+startQuest();
--- js/test.js
+++ js/test.js
@@ -1,3 +1,39 @@
-var quest = {
-    
-}
+var quest = {"heading":"Вопрос 1","question":"7 * 8 = ?","nextQuestion":"U2FsdGVkX19QHY6eUK9IvKpvjUPtXvVerbB+a6y/qP/9tCaKdIj09a91ZO+tcnah\nRFM+YHe/rfrDHrhHsEDsDee3MmUzbwp+wg52pchY0XAHn3ozTx8SddwnMceyXR5v\n1Cko4cquBniGYR9UTpa4lk9cxbdcx7oLMa+8NzeHSbAwjrwWJl/VDA0pI9+DGtR+\nMSpbZJxDmPneI1Q9PsdgD3cAnOoIOTK2nfYezYvHeF7hRhXO6KNUFLUze7m3kNvW\n0X1lUFb8acriEC0nWT/x8UCITEDAdgTJnkQGQWbnaKR8n75v7/xCebJvExWulQxj\nPyUEcbgWYqe9JJgrCzaZITG/TgdqPcF0kby55/nHxis+JkMczvwRJbOpw6EFODFH\nz+DZco/DpkoncDVirtPyryHBJDXJKYFbA1SCGUlmeGye1HRg0b/iMeE9W/d3i6nq\nFTRiPA9VYAIIoivRPGaU+DYiM4EHK6jt5P/VzsY7CZ6AGh2pTKp0XSREVC7dAi0N\nVAqYWgx+c9bIEAVPTIlg1/raJZWVASOZFkFOdrs3d6Tmcj75VkEJ1vbLAeBWdw6O\nrmq8eQMKxXjkOJi1+K4qZYk6MJZsEbKD89X0Pp9gmy01cE5KhRDzbn0FRwOqOOtG\n5F7AJtJRucZgXbMKVhbfzVc00Q/VbcfxuHlLOkZTfGq+v8pRDR4f3pXnn3qHsyx0\nt90swtvq/kp4Gx7xDGUT8v1XWvw9S+kwHONrU2J8tahuSDRvev6BEUNPneIfl8wc\n0cHxHRikSMvBCURaf5BiM5HX0jVmhCFQ9Yq6WDI3PK67qTd+wdvL0tYX7OV4n4bj\nSAW0YsvroqYCCb+LkyMBQL8OP4Nn7dxM/lTjH5fIq43R0OHBLpEGs9fqFsqP6PIa\nu7r4KMQUnjyJuKWyoj/YGH28gqDtOR9wRTXqDS/vIveW5e+uRG/EllBfh33pDThY\neAB86IcRVYkyyYY6ySf1gmDPOH+p+MoxkwDoG8QeHnNQHeFQWpLG+yjSBBPSL3ZK\nN0W5YK49fD9jPNHyikkbL7lU3Gn2+jX2RpNxnHt436W6EWMeDdzl1//5+Vq9AtYo\ncwNjiQX6varoFOKwBMhueb3HisaxZB2w76oCZi063eBDKC7Wm0pSPSM5jOoe2y5Z\n/qz9wkdGc+L9ibJyUY4s5AegNIMRJlLGW5H6+lOd1JfqR/YkawsqQNLD+3lJA+Rw\nLKRrbtcv3aKctx69GPu6j6Qn1j9Zyl/yavSJ7I5SburLWzmtq5rB5KGWeV69fA4u\nzKdWRDmfg09t+PmOa+zKo4HyHLwJRADo3rtr9t5R4C1wcw7iX/qmLuTJH3uXkEAi\nG2MudHNbiASuuMmGI8GAnVzUXwVD676bymyq8uRQjf5z3JxZm+9k1lhj1lecyZoG\nbH73TxbPQd/4zN04ZPpyrfeVvqS+lxBuobtOV37hpdrNF+QBKVWYinzVywo6v5j6\n2KQvvG9qN/uvc7aCtJNzAE+ob7s0t6JqutOz/O68isfTWMvn+aYX2hQTmA5U4e/q\nmz3kAWesGCfFPZjg3vGf9j2V6X2hhhLJ5Kpgbwa/sVB1TK7lNyT/mQZCEwk30hSw\nZPMp+RksiQzTLVR3ZvM07vFaXdNbTK3uMAWgvNnEVrGZmNCdNo/f3I7JWQEpoDMy\nOX7Cz5NgcH6L/gJytqw1R+50yVcgOU1tgRx5A6kN72GXsc4J142V093RklUseEWw\nlle0IP41WLK51D5OlDvF9AMy/SUInrE7QlUghtTWnjh39Whd7LlTHj9LNtEfzHC+\nVMc5iCvWc6L4MVwg4Dft7gsA4TRzz/a9vfHtfe50JbZDhBpXz9pZkMG+0Uzhk+qH\nkLOX2ehML/4z1uELxSdYumiEIwvTr1EGEiYUZK9Y5P8epHvKkNiaM84lMmX5qoDG\nJVfZb3zFovlHhFG0YenPeOUm2Baw2eTgmiQoSg5ixlgwCrSg26+VXptqLHXZT0UO\ntipHJourRwNqYz59mqsGGgb4Mdqj74MMyn6nXrmI7z0ZCRM8v/b6BhNWn1eTNzUv\n0dDqZH4TuIiutUCqVg9Gcw9Q8C69v/0g8sdEMlMHHbjButr7Bp/uEFvL+0QfskUe\nWLQvIAc7YuctR0u1fJaDtrfSN2UlYx74HNjGUtxefekxgaZSxmgrUrs/wmTshKMI\nXz0RCP7jYp5InEubR/+r3IXLvMVboDV6WLPtqi4D9K1ovM0CGnGMCqo3psupOwBS\nL+BetfeCBYrUU4zYMk1d11jzvXCptEJdLzNKghP6opw=","questionIndex":0} ;
+
+
+var making = [
+{
+    heading: "Вопрос 1",
+    question: "7 * 8 = ?",
+    answer: "56"
+},
+{
+    heading: "Вопрос 2",
+    question: "7 * 8 = ?",
+    answer: "56"
+},
+{
+    heading: "Вопрос 3",
+    question: "7 * 8 = ?",
+    answer: "56"
+},
+{
+    heading: "Вопрос 4",
+    question: "7 * 8 = ?",
+    answer: "56"
+},
+{
+    heading: "Вопрос 5",
+    question: "7 * 8 = ?",
+    answer: "56"
+},
+{
+    heading: "Вопрос 6",
+    question: "7 * 8 = ?",
+    answer: "56"
+},
+{
+    heading: "Вопрос 7",
+    question: "7 * 8 = ?",
+    answer: "56"
+}]
